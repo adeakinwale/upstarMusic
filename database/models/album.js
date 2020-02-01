919887cc5d41571cf1d7d1aaee324746f6d5a390
Admin <you@example.com>
2020-02-01 09:40:01 +0100

    mongo operations

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,16 @@
 // Todo: create Album Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number
+});
+
+// const Album = mongoose.model("album", AlbumSchema);
+// It's a sub document, Only a schema instead of a model
+module.exports = AlbumSchema;
